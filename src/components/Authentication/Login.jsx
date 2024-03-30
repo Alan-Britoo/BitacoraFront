40e6039f07cae995513c7d0660449d483f425a5c
Alan-Britoo <rotcarrillo_8@hotmail.com>
2024-03-30 16:08:06 -0500

    incono

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Icons } from "../Icons/Icons";
 import { useNavigate } from "react-router-dom";
+import dev from "/public/devchallenges.svg";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
@@ -59,11 +60,7 @@ export const Login = () => {
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="./public/devchallenges.svg"
-              alt="Workflow"
-            />
+            <img className="mx-auto h-12 w-auto" src={dev} alt="Workflow" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
               Login
             </h2>
--- src/components/Authentication/Register.jsx
+++ src/components/Authentication/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Icons } from "../Icons/Icons";
 import { useNavigate } from "react-router-dom";
+import dev from "/public/devchallenges.svg";
 
 export const Register = () => {
   const [email, setEmail] = useState("");
@@ -17,7 +18,7 @@ export const Register = () => {
       user: user,
     };
 
-    fetch("https://bitacoraback-production.up.railway.app/api/auth/register", {
+    fetch("https://bitacoraback-production.up.railway.app/api/auth/Register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -45,11 +46,7 @@ export const Register = () => {
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="./public/devchallenges.svg"
-              alt="Workflow"
-            />
+            <img className="mx-auto h-12 w-auto" src={dev} alt="Workflow" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
               Register
             </h2>
